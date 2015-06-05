67f8fe9359d2cdfcdc3c57c0171c093d70d42bdd
Pratyush Verma <freestyler@pratyushs-macbook-pro.local>
2015-06-06 05:27:44 +0530

    integrated new classification model

--- python/classify.py
+++ python/classify.py
@@ -2,6 +2,9 @@ from textblob.classifiers import NaiveBayesClassifier
 import argparse
 import utils
 import data
+import random
+from nltk.stem.snowball import SnowballStemmer
+
 
 CLASSIFIER_FILE_NAME = 'classifier.pickle'
 
@@ -10,28 +13,48 @@ def load_data(file_path):
 	with open(file_path) as f:
 		data = []
 		for line in f:
-			line = line.decode('utf-8')
+			line = line.decode('utf-8').strip()
 			if line:
 				data.append(tuple(line.rsplit(',',1)))
 	return data
 
 def get_features(document):
-	document_words = set(document)
+	document_words = set(document.split())
+	s = SnowballStemmer("english")
+	stemmed_words = [ s.stem(word) for word in document_words ]
 	features = {}
+	features['count'] = len(document_words)
 	for word in data.wordlist:
-		features['contains({})'.format(word)] = (word in document_words)
+		features['contains({})'.format(s.stem(word))] = (word in stemmed_words)
 	return features
 
 def train(file_path):
 	""" train the classifier using the provided file """
 	data = load_data(file_path)
-	#classifier = NaiveBayesClassfier(data,feature_extractor=get_features)
-	classifier = NaiveBayesClassifier(data)
+	random.shuffle(data)
+	print data
+	classifier = NaiveBayesClassifier(data,feature_extractor=get_features)
+	#classifier = NaiveBayesClassifier(data)
 	utils.store_classifier(classifier,CLASSIFIER_FILE_NAME)
+
+def train_n_test(file_path):
+	data = load_data(file_path)
+	random.shuffle(data)
+	train = data[0:80]
+	test = data[80:]
+	#classifier = NaiveBayesClassifier(train)
+	classifier = NaiveBayesClassifier(train,feature_extractor=get_features)
+	print classifier.accuracy(test)
 
 def classify_text(text):
 	 classifier = utils.load_classifier(CLASSIFIER_FILE_NAME)
-	 print classifier.classify(text)
+	 prob_dist = classifier.prob_classify(text)
+	 max_val =  prob_dist.max()
+	 print max_val
+	 print round(prob_dist.prob("general"), 2)
+	 print round(prob_dist.prob("app"), 2)
+	 print round(prob_dist.prob("pricing"), 2)
+	 print classifier.show_informative_features(5)  
 
 
 if __name__ == "__main__":
@@ -39,6 +62,7 @@ if __name__ == "__main__":
 	group = parser.add_mutually_exclusive_group(required=True)
 	group.add_argument("-t","--train",nargs=1,help="File path to train the classifier")
 	group.add_argument("-c","--classify",nargs=1,help="Text to be classified")
+	group.add_argument("-x","--test-accuracy",nargs=1,help="Test Accuracy")
 
 	args = parser.parse_args()
 
@@ -46,3 +70,5 @@ if __name__ == "__main__":
 		train(args.train[0])
 	elif args.classify:
 		classify_text(args.classify[0])
+	elif args.test_accuracy:
+		train_n_test(args.test_accuracy[0])
