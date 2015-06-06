4fbd412a54a1ec747e8a5ba82d756820c67dc7c4
Yashodhan <yashodhan.divakaran@webyog.com>
2015-06-06 12:33:06 +0530

    bar graph
    Signed-off-by: Yashodhan <yashodhan.divakaran@webyog.com>

--- FlipkartHackathonApp/app/src/main/java/flipkart/hackathon/com/flipkarthackathonapp/CityCategoriesActivityFragment.java
+++ FlipkartHackathonApp/app/src/main/java/flipkart/hackathon/com/flipkarthackathonapp/CityCategoriesActivityFragment.java
@@ -14,11 +14,15 @@ import com.github.mikephil.charting.data.BarEntry;
 import java.util.ArrayList;
 import java.util.List;
 
+import flipkart.hackathon.com.flipkarthackathonapp.asynctask.GetCategoriesFromDB;
+import flipkart.hackathon.com.flipkarthackathonapp.data.entities.Categories;
+import flipkart.hackathon.com.flipkarthackathonapp.data.entities.Cities;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class CityCategoriesActivityFragment extends Fragment {
+public class CityCategoriesActivityFragment extends Fragment implements GetCategoriesFromDB.RetrivalDoneCategories{
     View mainView;
     HorizontalBarChart barChart;
 
@@ -32,7 +36,29 @@ public class CityCategoriesActivityFragment extends Fragment {
         mainView = inflater.inflate(R.layout.city_categories_fragment_main, container, false);
         barChart = (HorizontalBarChart)mainView.findViewById(R.id.barChart);
 
+        barChart.setDescription("");
+        GetCategoriesFromDB task = new GetCategoriesFromDB(getActivity(),this,
+                new Cities(getActivity().getIntent().getStringExtra("city_name")));
+        task.execute();
+        return mainView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void updatedCategories(List<Categories> categories) {
+
         List<BarEntry> barEntries = new ArrayList<>();
+        List<String> xVals = new ArrayList<>();
+        int count =0;
+        for(Categories category: categories){
+            barEntries.add(new BarEntry(category.getCount(),count++));
+            xVals.add(category.getName());
+        }/*
         BarEntry entry1 = new BarEntry(40.0f,0);
         BarEntry entry2 = new BarEntry(10.0f,1);
         BarEntry entry3 = new BarEntry(30.0f,2);
@@ -43,28 +69,20 @@ public class CityCategoriesActivityFragment extends Fragment {
         barEntries.add(entry2);
         barEntries.add(entry3);
         barEntries.add(entry4);
-        barEntries.add(entry5);
+        barEntries.add(entry5);*/
 
         BarDataSet dataSet = new BarDataSet(barEntries,"No of Tweets");
 
-        String xVals[] = new String[5];
+       /* String [] = new String[5];
         xVals[0] = "Cat 1";
         xVals[1] = "Cat 2";
         xVals[2] = "Cat 3";
         xVals[3] = "Cat 4";
-        xVals[4] = "Cat 5";
+        xVals[4] = "Cat 5";*/
         BarData barData = new BarData(xVals,dataSet);
 
         barChart.setData(barData);
-        barChart.setDescription("");
-        return mainView;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        barChart.invalidate();
 
     }
-
-
 }
--- FlipkartHackathonApp/app/src/main/java/flipkart/hackathon/com/flipkarthackathonapp/asynctask/GetCategoriesFromDB.java
+++ FlipkartHackathonApp/app/src/main/java/flipkart/hackathon/com/flipkarthackathonapp/asynctask/GetCategoriesFromDB.java
@@ -1,7 +1,43 @@
 package flipkart.hackathon.com.flipkarthackathonapp.asynctask;
 
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import flipkart.hackathon.com.flipkarthackathonapp.data.FKDBWrapper;
+import flipkart.hackathon.com.flipkarthackathonapp.data.entities.Categories;
+import flipkart.hackathon.com.flipkarthackathonapp.data.entities.Cities;
+
 /**
  * Created by webyog on 06/06/15.
  */
-public class GetCategoriesFromDB {
+public class GetCategoriesFromDB extends AsyncTask<Void,Void,List<Categories>>{
+    private Cities city;
+
+
+    public GetCategoriesFromDB(Context mContext, RetrivalDoneCategories mListener,Cities city) {
+        this.mContext = mContext;
+        this.mListener = mListener;
+        this.city = city;
+    }
+
+    private Context mContext;
+    private RetrivalDoneCategories mListener;
+
+
+    public interface RetrivalDoneCategories {
+        void updatedCategories(List<Categories> categories);
+    }
+
+    @Override
+    protected List<Categories> doInBackground(Void... params) {
+        FKDBWrapper dbWrapper = new FKDBWrapper(mContext);
+        return dbWrapper.getCategories(city);
+    }
+
+    @Override
+    protected void onPostExecute(List<Categories> categorieses) {
+        mListener.updatedCategories(categorieses);
+    }
 }
